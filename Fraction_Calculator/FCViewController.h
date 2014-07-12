f8d86674cae6d6705342ef201dfae083deb5871d
jclish <jourdan@clish.com>
2014-07-11 18:49:15 -0700

    Third version

--- Fraction_Calculator/FCViewController.h
+++ Fraction_Calculator/FCViewController.h
@@ -10,4 +10,25 @@
 
 @interface FCViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UILabel *display;
+
+-(void) processDigit: (int) digit;
+-(void) processOp: (char) theOp;
+-(void) storeFracPart;
+
+//numeric keys
+-(IBAction) clickDigit: (UIButton *) sender;
+
+// Arithmatic Operation Keys
+
+-(IBAction)clickPlus;
+-(IBAction)clickMinus;
+-(IBAction)clickMultiply;
+-(IBAction)clickDivide;
+
+// Misc Keys
+-(IBAction)clickOver;
+-(IBAction)clickEquals;
+-(IBAction)clickClear;
+
 @end
