37535c9ac69d21d43b751a3f5e549276b744b663
jclish <jourdan@clish.com>
2014-07-11 18:57:17 -0700

    More changes

--- Fraction_Calculator/FCViewController.m
+++ Fraction_Calculator/FCViewController.m
@@ -7,23 +7,88 @@
 //
 
 #import "FCViewController.h"
-
-@interface FCViewController ()
-
-@end
+#import "Calculator.h"
 
 @implementation FCViewController
+{
+    char            op;
+    int             currentNumber;
+    BOOL            firstOperand, isNumerator;
+    Calculator      *myCalculator;
+    NSMutableString *displayString;
+}
+@synthesize display;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    firstOperand = YES;
+    isNumerator=YES;
+    displayString=[NSMutableString stringWithCapacity:40];
+    myCalculator=[[Calculator alloc] init];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void) processDigit: (int) digit
+{
+    currentNumber=currentNumber* 10 + digit;
+    
+    [displayString appendString:
+     [NSString stringWithFormat:@"%i", digit]];
+    display.text=displayString;
+}
+
+-(void) processOp: (char) theOp
+{
+    
+}
+
+-(void) storeFracPart
+{
+    
+}
+
+-(IBAction) clickDigit: (UIButton *) sender
+{
+    
+}
+
+
+-(IBAction)clickPlus
+{
+    
+}
+-(IBAction)clickMinus
+{
+    
+}
+-(IBAction)clickMultiply
+{
+    
+}
+-(IBAction)clickDivide
+{
+    
+}
+
+
+-(IBAction)clickOver
+{
+    
+}
+-(IBAction)clickEquals
+{
+    
+}
+-(IBAction)clickClear
+{
+    
 }
 
 @end
