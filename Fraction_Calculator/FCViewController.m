ef6a0b8319bc2816929bd93fd51d372ce65b38b9
jclish <jourdan@clish.com>
2014-07-11 19:06:20 -0700

    just before UI

--- Fraction_Calculator/FCViewController.m
+++ Fraction_Calculator/FCViewController.m
@@ -44,51 +44,115 @@
     display.text=displayString;
 }
 
+-(IBAction) clickDigit: (UIButton *) sender
+{
+    int digit = sender.tag;
+    
+    [self processDigit: digit];
+}
+
 -(void) processOp: (char) theOp
 {
+    NSString    *opStr;
     
+    op=theOp;
+    
+    switch (theOp) {
+        case '+':
+            opStr=@" + ";
+            break;
+        case '-':
+            opStr=@" - ";
+            break;
+        case '*':
+            opStr=@" X ";
+            break;
+        case '/':
+            opStr=@" ÷ ";
+            break;
+        default:
+            break;
+    }
+    [self storeFracPart];
+    firstOperand = NO;
+    isNumerator = YES;
+    
+    [displayString appendString: opStr];
+    display.text = displayString;
 }
 
 -(void) storeFracPart
 {
-    
+    if (firstOperand) {
+        if (isNumerator) {
+            myCalculator.operand1.numerator = currentNumber;
+            myCalculator.operand1.denominator = 1; // e.g. 3 * 4/5 =
+        }
+        else
+            myCalculator.operand1.denominator = currentNumber;
+    }
+    else if (isNumerator) {
+        myCalculator.operand2.numerator = currentNumber;
+        myCalculator.operand2.denominator = 1; // e.g. 3/2 * 4 =
+    }
+    else {
+        myCalculator.operand2.denominator = currentNumber;
+        firstOperand = YES;
+    }
+    currentNumber = 0;
 }
-
--(IBAction) clickDigit: (UIButton *) sender
-{
-    
-}
-
 
 -(IBAction)clickPlus
 {
-    
+    [self processOp: '+'];
 }
 -(IBAction)clickMinus
 {
-    
+    [self processOp: '-'];
 }
 -(IBAction)clickMultiply
 {
-    
+    [self processOp: '*'];
 }
 -(IBAction)clickDivide
 {
-    
+    [self processOp: '/'];
 }
-
 
 -(IBAction)clickOver
 {
-    
+    [self storeFracPart];
+    isNumerator = NO;
+    [displayString appendString: @"/"];
+    display.text = displayString;
 }
+
 -(IBAction)clickEquals
 {
-    
+    if ( firstOperand == NO ) {
+        [self storeFracPart];
+        [myCalculator performOperation: op];
+        
+        [displayString appendString: @" = "];
+        [displayString appendString: [myCalculator.accumulator convertToString]];
+        display.text = displayString;
+        
+        currentNumber = 0;
+        isNumerator = YES;
+        firstOperand = YES;
+        [displayString setString: @""];
+    }
 }
+
 -(IBAction)clickClear
 {
+    isNumerator = YES;
+    firstOperand = YES;
+    currentNumber = 0;
+    [myCalculator clear];
     
+    [displayString setString: @""];
+    display.text = displayString;
 }
 
 @end
